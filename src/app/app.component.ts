452a389d50b545eee6654326598c747d7e77c22a
Osama Aleem <aleemosama10@gmail.com>
2023-11-19 01:01:14 +0500

    basic-app-working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>  
+  </main>`,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'home-app';
